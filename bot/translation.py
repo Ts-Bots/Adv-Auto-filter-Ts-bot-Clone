3e5d9c2e79c3b25689086e63bf685469dc5a5ea9
Ts_Bots <81805185+Ts-Bots@users.noreply.github.com>
2021-04-26 12:27:37 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -1,58 +1,69 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# (c) @AlbertEinsteinTG
+# (c) @Hillard_Har 
 
 class Translation(object):
     
-    START_TEXT = """<b>Hey {}!!</b>
-<i>Am Just A Advance Auto Filter Bot....😉
+    START_TEXT = """😃 ʜᴀɪ {} ,
 
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
+ɪ ᴀᴍ ᴀ ᴀᴜᴛᴏ ғɪʟᴛᴇʀ ʙᴏᴛ sɪᴍᴘʟʏ sᴇᴀʀᴄʜ ғᴏʀ ᴛʜᴇ ғɪʟᴇs ᴍᴀᴅᴇ ғᴏʀ <a href="https://t.me/Anylink_infinity_Movies">ᴀɴʏʟɪɴᴋ ɪɴғɪɴɪᴛʏ ᴍᴏᴠɪᴇs</a> ᴀᴄᴄᴏʀᴅɪɴɢ ᴛᴏ ɢɪᴠᴇɴ ᴏ̨ᴜᴇʀʏ ᴀɴᴅ ɢɪᴠᴇs ʟɪɴᴋ ᴛᴏ ᴛʜᴏsᴇ ғɪʟᴇs ᴀs ʙᴜᴛᴛᴏɴs
 
-For More Details Click Help Button Below..
-@CrazyBotsz
-</i>"""    
+🔱 ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ 👉 @Hillard_Har
+
+"""   
     
     HELP_TEXT = """
-<b><i><u>How To Use Me!?</u></i></b>
+<u>💡 𝐇𝐞𝐥𝐩</u>
 
 <i>
--> Add Me To Any Group And Make Me Admin
--> Add Me To Your Desired Channel
+📌 Add Me To Any Group And Make Me Admin
+📌 Add Me To Your Desired Channel
 </i>
 
-<b>Bot Commands (Works Only In Groups) :</b>
+<b>🔰 𝐌𝐲 𝐂𝐨𝐦𝐦𝐚𝐧𝐝𝐬 (Works Only In Groups) :</b>
 
-    -> <code>/add chat_id</code>
+    👉 <code>/add chat_id</code>
                 OR                  - To Connect A Group With A Channel (Bot Should Be Admin With Full Previlages In Both Group And Channel)
      <code>/add @Username</code>
      
-    -> <code>/del chat_id</code>
+    👉 <code>/del chat_id</code>
                 OR                  - To disconnect A Group With A Channel
      <code>/del @Username</code>
      
-    -> <code>/delall</code>  - This Command Will Disconnect All Connected Channel With The Group And Deletes All Its File From DB
+    👉 <code>/delall</code>  - This Command Will Disconnect All Connected Channel With The Group And Deletes All Its File From DB
     
-    -> <code>/settings</code> -  This Command Will Display You A Settings Pannel Instance Which Can Be Used To Tweek Bot's Settings Accordingly
+    👉 <code>/settings</code> -  This Command Will Display You A Settings Pannel Instance Which Can Be Used To Tweek Bot's Settings Accordingly
 
-            -> <code>Channel</code> - Button Will Show You All The Connected Chats With The Group And Will Show Buttons Correspnding To There Order For Furthur Controls
-            
-            -> <code>Filter Types</code> - Button Will Show You The 3 Filter Option Available In Bot... Pressing Each Buttons Will Either Enable or Disable Them And This Will Take Into Action As Soon As You Use Them Without The Need Of A Restart
+🔰 𝐒𝐄𝐓𝐓𝐈𝐍𝐆 𝐏𝐀𝐍𝐄𝐋
 
-            -> <code>Configure</code> - Button Will Helps You To Change No. of Pages/ Buttons Per Page/ Total Result Without Acutally Editing The Repo... Also It Provide Option To Enable/Disable For Showing Invite Link In Each Results
+            👉 <code>Channel</code> - Button Will Show You All The Connected Chats With The Group And Will Show Buttons Correspnding To There Order For Furthur Controls
             
-            -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
+            👉 <code>Filter Types</code> - Button Will Show You The 3 Filter Option Available In Bot... Pressing Each Buttons Will Either Enable or Disable Them And This Will Take Into Action As Soon As You Use Them Without The Need Of A Restart
+
+            👉 <code>Configure</code> - Button Will Helps You To Change No. of Pages/ Buttons Per Page/ Total Result Without Acutally Editing The Repo... Also It Provide Option To Enable/Disable For Showing Invite Link In Each Results
             
-@CrazyBotsz
+            👉 <code>Status</code> - Button Will Shows The Stats Of Your Channel
+            
+<b><a href="https://t.me/ts_bots">©️ ᴛʀᴀᴄᴋsᴛᴜᴅɪᴏ's ʙᴏᴛs</a></b>
 """
     
-    ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
-    
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+    ABOUT_TEXT = """📕 𝐀𝐛𝐨𝐮𝐭 𝐌𝐞 ,
+\n○ ᴍʏ ɴᴀᴍᴇ : 𝙻𝚒𝚗𝚔𝚎𝚛
 
-<b>➥ Language</b> : <code>Python3</code>
+○ ʟᴀɴɢᴜᴀɢᴇ : ᴘʏᴛʜᴏɴ 
 
-<b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
+○ ғʀᴀᴍᴇᴡᴏʀᴋ : ᴘʏʀᴏɢʀᴀᴍ 
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
-"""
+○ sᴇʀᴠᴇʀ : ʜᴇʀᴏᴋᴜ 
+
+○ ᴠᴇʀsɪᴏɴ : 1.0.1
+
+○ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ : 🔐
+
+○ ᴄʀᴇᴀᴛᴏʀ : [ᴛʀᴀᴄᴋsᴛᴜᴅɪᴏ](https://t.me/trackstudio) & [ʜɪʟʟᴀʀᴅ_ʜᴀʀ](https://t.me/Hillard_Har)
+
+**ᴜᴘᴅᴀᴛᴇᴅ ᴏɴ 19-4-21 ɪɴᴅɪᴀɴ ᴛɪᴍᴇ 12:27 ᴘᴍ**"""
+
+# ᴘᴍ
+# ᴀᴍ 
+
