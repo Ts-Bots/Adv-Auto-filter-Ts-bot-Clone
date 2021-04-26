aad6339a9898aaa2d98e65338542764fe01fa826
Ts_Bots <81805185+Ts-Bots@users.noreply.github.com>
2021-04-26 12:20:20 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# (c) @AlbertEinsteinTG
+# (c) @AlbertEinsteinTG | @Hillard_Har  
 
 from pyrogram import filters, Client
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
